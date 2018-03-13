34297988997d0b452789be55acbc5d73f5ce686c
Apolishgal <36082393+Apolishgal@users.noreply.github.com>
2018-03-13 12:24:18 -0400

    Update testplayer.py

--- testplayer.py
+++ testplayer.py
@@ -37,3 +37,54 @@ if __name__ == '__main__':
     win.resize(200, 100)
     win.show()
     sys.exit(app.exec_())
+    
+    
+    
+    
+    
+    #This program is seperate from the one above. The first one is the working player, but without using Qt designer. The bottom one is my attempt to combine the two, but it is not fully working. 
+    #When I click the play button, it changes from play to pause but then an error comes up and the computer files do not open in order to choose and play the .mp3 file. 
+    
+    
+   
+    
+    
+import sys
+from PyQt4 import QtCore, QtGui, uic
+import easygui
+import random
+    
+form_class = uic.loadUiType("MediaPlayer.ui") [0]
+    
+    class MyWindowClass(QtGui.QMainWindow, form_class):
+        def __init__(self, parent = None):
+            QtGui.QMainWindow.__init__(self, parent)
+            self.setupUi(self)
+            self.pushButton.clicked.connect(self.button_clicked)
+            
+            def button_clicked(self, parent = None):
+                if(self.Name.text () == "choose song from playlist"):
+                    pygame.mixer.init()
+                    pygame.mixer.music.load("musicfile.mp3")
+                    pygame.mixer.music.play()
+                    
+        def button_clicked(self):
+            if(self.pushButton.text() == "Play"):
+                self.pushButton.setText("Pause")
+                if self.mediaObject.state() == Phonon.PlayingSTate:
+                    self.mediaObject.stop()
+                else:
+                    path = QtGui.QFileDialog.getOpenFileName(self, self.text())
+             if path:
+                self.mediaObject.setCurrentSource(Phonon.MediaSource(path))
+                self.mediaObject.play()
+            else:
+                self.pushButton.setText("Play")
+                print("Testing...")
+                print("It looks like it's working")
+       
+app = QtGui.QApplication(sys.argv)
+myWindow = MyWindowClass(None)
+myWindow.show()
+app.exec_()
+        
